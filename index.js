10f6eabd73b15fb21ac47e2115c82bcd4c87cd4a
Chi Nguyen Ngoc <ngocchi.fit148.vnu@gmail.com>
2020-11-24 16:29:12 +0700

    couchdb tool v2 with another insert multi function (using promise chaining)

--- index.js
+++ index.js
@@ -3,61 +3,150 @@ const nano = require("nano")("http://admin:admin@localhost:5984");
 const process = require("process");
 
 function main() {
-  const prompt = require('prompt-sync')({sigint: true});
+  const prompt = require("prompt-sync")({ sigint: true });
   console.log("Please enter link of your file");
-  let file_name = prompt('>');
+  let file_name = prompt(">");
   console.log("wait.....");
   insertMulti(file_name);
 }
 
-
 function testFile(path) {
-  return new Promise(function(resolve, reject) {
-      fs.readFile(path, (err, data) => {
-          if (!err) {
-              resolve(data);
-          }
-          else {
-              reject(err);
-          }
-      })
-})
+  return new Promise(function (resolve, reject) {
+    fs.readFile(path, (err, data) => {
+      if (!err) {
+        resolve(data);
+      } else {
+        reject(err);
+      }
+    });
+  });
 }
-
+// this version of insert multi will not return a promise
 function insertMulti(path) {
-  let person = nano.use('person');
-  var start ;
-  var hrStart;
-  testFile(path).then((data) => {
-    start = new Date();
-    hrStart = process.hrtime();
-    return person.bulk(JSON.parse(data))
-  }).then((res) => {
+  let person = nano.use("person");
+  let start;
+  let hrStart;
+  testFile(path)
+    .then((data) => {
+      start = new Date();
+      hrStart = process.hrtime();
+      return person.bulk(JSON.parse(data));
+    })
+    .then((res) => {
       var end = new Date() - start;
       var hrEnd = process.hrtime(hrStart);
       console.info("Execution time: %dms", end);
-      console.info('Execution time (hr): %ds %dms', hrEnd[0], hrEnd[1] / 1000000);
+      console.info(
+        "Execution time (hr): %ds %dms",
+        hrEnd[0],
+        hrEnd[1] / 1000000
+      );
       console.log("done");
-  }).catch((err) => {
+    })
+    .catch((err) => {
       var end = new Date() - start;
       var hrEnd = process.hrtime(hrStart);
       console.info("Execution time: %dms", end);
-      console.info('Execution time (hr): %ds %dms', hrEnd[0], hrEnd[1] / 1000000);
+      console.info(
+        "Execution time (hr): %ds %dms",
+        hrEnd[0],
+        hrEnd[1] / 1000000
+      );
       console.log("err");
-  }) 
+    });
+}
+// this version of insert multi will return a promise
+function insertMultiPromise(path) {
+  let person = nano.use("person");
+  let fileContent = fs.readFileSync(path);
+  return new Promise((resolve, reject) => {
+    person
+      .bulk(JSON.parse(fileContent))
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
+// this function insert all of file in a directory but sometimes it raise error???
+async function insertMultiFolder(path) {
+  const dir = await fs.promises.opendir(path);
+  let start = new Date();
+  let hrStart = process.hrtime();
+  let end;
+  let hrEnd;
+  for await (const dirent of dir) {
+    insertMultiPromise(path + dirent.name)
+      .then((res) => {
+        end = new Date() - start;
+        hrEnd = process.hrtime(hrStart);
+        console.info("Execution time: %dms", end);
+        console.info(
+          "Execution time (hr): %ds %dms",
+          hrEnd[0],
+          hrEnd[1] / 1000000
+        );
+        console.log("done");
+      })
+      .catch((err) => {
+        end = new Date() - start;
+        hrEnd = process.hrtime(hrStart);
+        console.info("Execution time: %dms", end);
+        console.info(
+          "Execution time (hr): %ds %dms",
+          hrEnd[0],
+          hrEnd[1] / 1000000
+        );
+        console.log("err");
+      });
+  }
 }
 
+// another verson of above function
 
-async function insertMultiFolder(path) {
+async function insertMultiFolderOld(path) {
   const dir = await fs.promises.opendir(path);
   for await (const dirent of dir) {
     insertMulti(path + dirent.name);
   }
 }
 
-insertMultiFolder('./test-data/');
+// insertMultiFolder('./data-online/');
 
+// const arr = [0, 1, 2, 3, 4]
+
+// for await (const i of arr) {
+//   main();
+// }
 
 // main();
-
-
+let start = new Date();
+let hrStart = process.hrtime();
+let end;
+let hrEnd;
+insertMultiPromise("./test-data/data3.json")
+  .then((res) => {
+    return insertMultiPromise("./test-data/data4.json");
+  })
+  .then((res) => {
+    return insertMultiPromise("./test-data/data5.json");
+  })
+  .then((res) => {
+    return insertMultiPromise("./test-data/data6.json");
+  })
+  .then((res) => {
+    end = new Date() - start;
+    hrEnd = process.hrtime(hrStart);
+    console.info("Execution time: %dms", end);
+    console.info("Execution time (hr): %ds %dms", hrEnd[0], hrEnd[1] / 1000000);
+    console.log("done");
+  })
+  .catch((err) => {
+    end = new Date() - start;
+    hrEnd = process.hrtime(hrStart);
+    console.info("Execution time: %dms", end);
+    console.info("Execution time (hr): %ds %dms", hrEnd[0], hrEnd[1] / 1000000);
+    console.log("err");
+  });
--- testFunc.js
+++ testFunc.js
@@ -6,5 +6,36 @@ async function print(path) {
     console.log(path + dirent.name);
   }
 }
-print('./data/10M-6/1M-1/').catch(console.error);
+// print('./data/10M-6/1M-1/').catch(console.error);
+
+// Node program to demonstrate the 
+// dir.path() API 
+
+// Intiating asyn function 
+async function stop(path) { 
+
+// Creating and initiating directory's 
+// underlying resource handle 
+const dir = await fs.promises.opendir(path); 
+
+// Getting all the dirent of the directory 
+for (var i = 1 ; i<=2 ; i++) { 
+
+	// Reading each dirent one by one 
+	// by using read() method 
+	dir.read( (err, dirent) => { 
+
+	// Display each dirent one by one 
+	console.log(`${dirent.name} ${err ? 'does not exist' : 'exists'}`); 
+	}); 
+} 
+} 
+
+// Catching error 
+// stop('./data').catch(console.error); 
+
+let f = fs.readFileSync('./data-online/test-data.json');
+console.log(JSON.parse(f));
+console.log(JSON.parse(f).docs.length);
+
 
